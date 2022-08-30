145025f09841d3876c49e05b27b53653eb7a1676
김민주 <77184157+kim-min-ju449@users.noreply.github.com>
2022-08-30 13:51:19 +0900

    8.30

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import ToDoForm from "./component/ToDoForm";
 import './App.css';
 import ToDoList from "./component/ToDoList";
-
+import ToDo from "./component/ToDo"
+import {useState, useRef, useCallback} from 'react';
 
 
 const App =()=>{
+  const [todos, setTodos] = useState([
+
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+      text =>{
+        const todo = {
+          id: nextId.current,
+          text,
+          checked: true,
+        };
+        setTodos(todos.concat(todo));
+        nextId.current +=1;
+      },
+      [todos],
+  );
+
+  const onToggle = useCallback(
+      id => {
+        setTodos(
+            todos.map(todo =>
+                todo.id === id ? { ...todo, checked: !todo.checked}: todo,
+            ),
+        );
+      },
+      [todos],
+  )
   return (
     <div>
-      {/*<ToDoForm/>*/}
-        <ToDoList/>
+      <ToDoForm onInsert={onInsert}/>
+        <ToDoList props={setTodos} onToggle={onToggle}/>
+        <ToDo/>
 
     </div>
   );
